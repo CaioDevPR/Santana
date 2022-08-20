ea218439de63c06a57c7adef77bd190d24e7cc52
devcaio <programadorpr@outlook.com>
2022-08-20 20:53:44 -0300

    atualizacoes , teste implamtados de responsividade

--- main.js
+++ main.js
@@ -8,3 +8,44 @@ function menuShow(){
      ul.classList.add('open');
     }
 }
+
+
+const target = document.querySelectorAll('[data-anime');
+const animationClass = 'animate';
+
+function animeScroll() { 
+const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+target.forEach(function(element){
+    if((windowTop) > element.offsetTop){
+        element.classList.add(animationClass);
+    } else {
+        element.classList.remove(animationClass);
+    }
+})
+}
+
+
+animeScroll();
+
+if(target.length) {
+    window.addEventListener('scroll' , debounce(function() {
+        animeScroll();
+    }, 200));
+}
+
+
+
+
+window.addEventListener('scroll'), function() {
+    animeScroll();
+}
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('galeria').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
